fb357d8ba5176dae8be5f88fa6276340c9dc7b4d
NguyenDangMinhThanh2004 <Nguyendangminhthanh2004@gmail.com>
2026-03-20 15:59:55 +0700

    gi do

---
+++ src/main/java/com/example/NGUYENTHANH/NguyenthanhApplication.java
@@ -0,0 +1,13 @@
+package com.example.NGUYENTHANH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenthanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenthanhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NGUYENTHANH/NguyenthanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NGUYENTHANH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenthanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
